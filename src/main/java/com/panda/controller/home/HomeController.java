cc2d53c8048cc319d8092d4fbf38794c72b740f5
Alan <jionlandry@gmail.com>
2017-11-21 18:36:35 +0800

    测试JSTree

--- src/main/java/com/panda/controller/home/HomeController.java
+++ src/main/java/com/panda/controller/home/HomeController.java
@@ -1,24 +1,23 @@
 package com.panda.controller.home;
 
 import com.panda.config.DruidConfig;
-import com.panda.model.system.Menu;
 import com.panda.model.system.Users;
-import com.panda.service.system.UsersService;
-import com.panda.util.SerializeUtil;
+import com.panda.service.system.*;
+import com.panda.util.ResultMsgUtil;
+import com.panda.util.ResultStateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
+import java.util.Map;
 
 /**
  * Created with IDEA.
@@ -30,23 +29,43 @@ import java.util.UUID;
 public class HomeController {
 
     private Logger logger = LoggerFactory.getLogger(DruidConfig.class);
-
+    // logger.info("jedisPool uuid : " + user.getId());
+    // try (Jedis jedis = jedisPool.getResource()) {
+    //jedis.set("f9222c95-cf5a-4036-8ad9-ecface0aaf83".getBytes(), SerializeUtil.serialize(user));
+    // jedis.get("f9222c95-cf5a-4036-8ad9-ecface0aaf83");
+    //byte[] reduser = jedis.get("f9222c95-cf5a-4036-8ad9-000000000000".getBytes());
+    // return (ArrayList) SerializeUtil.unserialize(reduser);
+    // }
     @Autowired
     private JedisPool jedisPool;
 
     @Resource
     private UsersService usersService;
 
+    @Resource
+    private RolesService rolesService;
+
+    @Resource
+    private RoleMenuService roleMenuService;
+
+    @Resource
+    private PageRoleService pageRoleService;
+
+    @Resource
+    private DictionaryService dictionaryService;
+
+    private static boolean message = false;
+    private static Object  data    = null;
+
     @RequestMapping({"/","/thy"})
     public String thymeleaf(){
 
         return "home/thymeleaf";
     }
-    @RequestMapping(value = "/hello1")
-    @ResponseBody
-    public Users hello1(){
-        Users user = usersService.selectByPrimaryKey("0f1443aa-eade-410d-b8bf-74ebfa914ca4");
-        return user;
+    @RequestMapping(value = "/index")
+    public String index(){
+        //Users user = usersService.selectByPrimaryKey("0f1443aa-eade-410d-b8bf-74ebfa914ca4");
+        return "home/index-select";
     }
 
     @RequestMapping(value = "getUser")
@@ -55,28 +74,47 @@ public class HomeController {
         return usersService.selectByPrimaryKey("0f1443aa-eade-410d-b8bf-74ebfa914ca4");
     }
 
-    @RequestMapping(value = "/add")
+    @RequestMapping(value = "/jsTreeSelect",method = RequestMethod.GET)
     @ResponseBody
-    public int add(){
-        Users user = new Users();
-        user.setId(UUID.randomUUID().toString());
-        user.setAccount("Alan");
-        user.setChineseName("哗哗");
-        user.setLastDate(new Date());
-        user.setMobile("13713666160");
-        user.setStatus(0);
-        user.setModifyTime(new Date());
-        user.setEmail("2755527207@qq.com");
-        user.setLoginType(1);
-        user.setUserType(2);
-        return usersService.insert(user);
-
-       // logger.info("jedisPool uuid : " + user.getId());
-       // try (Jedis jedis = jedisPool.getResource()) {
-            //jedis.set("f9222c95-cf5a-4036-8ad9-ecface0aaf83".getBytes(), SerializeUtil.serialize(user));
-           // jedis.get("f9222c95-cf5a-4036-8ad9-ecface0aaf83");
-            //byte[] reduser = jedis.get("f9222c95-cf5a-4036-8ad9-000000000000".getBytes());
-           // return (ArrayList) SerializeUtil.unserialize(reduser);
-       // }
+    public Object jsTreeSelect(){
+        message = false;
+        data    = null;
+        String menuId = "caf65c04-beed-11e7-959c-38d547b81379";
+        String roleId = "f8692cd2-e801-11e4-8fee-40167e64eefd";
+        Integer status = 1;
+        try {
+            if(!menuId.isEmpty() && !roleId.isEmpty() && status == 1){
+                Map<String,Object> map = new HashMap<>();
+                map.put("status",status);
+                map.put("parentId","b6315b3a-1587-11e5-a9de-000c29d7a3a0");
+                List<Map<String,Object>> dictionaryList = dictionaryService.selectDictionaryListMap(map);
+                if(dictionaryList.size() > 0){
+                    map.put("parentId",null);
+                    map.put("menuId",menuId);
+                    map.put("roleId",roleId);
+                    for (Map<String,Object> item : dictionaryList){
+                        map.put("dictId",item.get("id"));
+                        Map<String,Object> pageRole = pageRoleService.selectPageRoleListByDictionary(map);
+                        if(pageRole != null){
+                            item.putAll(pageRole);
+                        }else{
+                            item.put("pageRoleStatus","3");
+                            item.put("pageRoleId",0);
+                            item.put("pageRoleDictId",0);
+                        }
+                    }
+                    message = true;
+                    data = dictionaryList;
+                }else{
+                    data = ResultStateUtil.NO_MORE_DATA;
+                }
+            }else{
+                data    = ResultStateUtil.ERROR_PARAMETER_IS_EMPTY;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            data    = ResultStateUtil.ERROR_DATABASE_OPERATION;
+        }
+        return ResultMsgUtil.getResultMsg(message,data);
     }
 }
